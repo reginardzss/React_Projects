4af1c42eb26aa421773443f1183e98919bc90b45
Akemi <akemi@Akemis-MacBook-Air.local>
2022-08-25 17:03:47 -0500

    Update comments

--- csv-read-convert-final/src/App.js
+++ csv-read-convert-final/src/App.js
@@ -1,3 +1,10 @@
+/* App that calls components
+
+Ana Paula Katsuda - A01025303
+Regina Rodriguez - A01284329
+Salvador Federico Milanes - A01029956 */
+
+
 import './App.css';
 import { useState } from 'react';
 import AskCSV from './components/AskCSV';
@@ -29,3 +36,7 @@ function App() {
 }
 
 export default App;
+
+/*Disclaimer: The current app was created based on the class topics, 
+internet tutorials and Gil Echeverría's examples in class. For 
+references to the tutorials, view each file. */
